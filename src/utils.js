39434c1585b87293b94d601d8b42b24321350f30
supersheykh <bahsekousidi@gmail.com>
2024-09-12 10:46:47 +0000

    before deleting the project folder

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,26 @@
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
-  export {shuffle} 
+  return array;
+}
+
+// IT TOGGLES THE MODE SO ANSWERS GET SHOWN OR NOT;
+const togglePlayMode = (a) => setPlayMode(a);
+const toggleShowMode = (a) => setShowMode(a);
+const toggleFetchMode = (a) => setFetchMode(a);
+
+export { shuffle, togglePlayMode, toggleShowMode, toggleFetchMode };
